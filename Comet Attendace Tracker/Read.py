e9cdc9423d9815a2d449e42d1f614913997fc391
Randi Faustino Roxas <randito-roxasjr@dlsu.edu.ph>
2017-09-26 23:29:08 +0800

    hello

--- Comet Attendace Tracker/Read.py
+++ Comet Attendace Tracker/Read.py
@@ -7,11 +7,18 @@ import signal
 import xlwt, xlrd
 import time
 from datetime import datetime
+import calendar
 from xlutils.copy import copy
 
 continue_reading = True
 ########################################################################################
 #fonts and styles
+boldtext = xlwt.Font()
+boldtext.colour_index = 0
+boldtext.bold = True
+label = xlwt.XFStyle()
+label.font = boldtext
+
 font = xlwt.Font()
 font.name = 'Times New Roman'
 font.colour_index = 2
@@ -24,7 +31,7 @@ font1.bold = False
 
 #Date format
 style = xlwt.XFStyle()
-style.num_format_str = 'D-MMMM-YY'
+style.num_format_str = 'D-MMMM-YYYY'
 style.font = font
 
 #Time format
@@ -36,23 +43,25 @@ style1.font = font1
 database = xlrd.open_workbook('DB.xlsx')
 data_sheet = database.sheet_by_name('Master List')
 id_num = 0
-def check_List(uid): #Checks if the uid is part of the database
 
+def check_List(uid): #Checks if the uid is part of the database
     column = data_sheet.col_values(colx=1, start_rowx=1)
-    
+
     if (str(uid)[1:-1]) in list(column):
-        
         return True
     else:
         return False
-  
-def create_attendace_sheet(): 
+
+def create_attendace_sheet(uid): #Use only ONCE per day
     attendance = xlwt.Workbook()
-    ws = attendace.add_sheet('Monday')
-    
+    date_now = datetime.today()
+    day = str(calendar.day_name[date_now.weekday()])
+
+    ws = attendance.add_sheet(day)
+
     row = 2
     col = 1
-    
+
     #Resize Columns
     col_2 = ws.col(1)
     col_2.width = 256*30
@@ -60,7 +69,9 @@ def create_attendace_sheet():
     col_3.width = 256*30
     col_4 = ws.col(3)
     col_4.width = 256*30
-    
+    col_5 = ws.col(4)
+    col_5.width = 256*30
+
     #Add Column Labels
     ws.write(0,0,'DATE', label)
     ws.write(0,1, datetime.today(), style)
@@ -68,21 +79,21 @@ def create_attendace_sheet():
     ws.write(row,2,'NAME', label)
     ws.write(row,3,'TIME-IN', label)
     ws.write(row,4,'TIME-OUT', label)
-    row += 1
-    
-    #Search ID number
-    
-    #Write Time in        
-    if
-        ws.write(row, col, datetime.time(datetime.now()), style1)
-    
-    
-    
 
-#def user_log(uid): #Writes the time in and time out of the uid
- #   column = data_sheet.col_values(colx=3
-    
+    attendance.save(day+' '+'Attendance.xlsx')
 
+def get_Id(uid): #Gets the ID number of the comet member
+    uid_column = data_sheet.col_values(colx=1, start_rowx=1)
+    id_column = data_sheet.col_values(colx=3, start_rowx=1)
+
+    if (str(uid)[1:-1]) in list(uid_column):
+        uid_temp = list(uid_column).index(str(uid)[1:-1])
+        return int(list(id_column)[uid_temp])
+    else:
+        return False
+
+def user_log(uid, row): #Writes the time in and time out of the uid
+    print('False')
 
 # Capture SIGINT for cleanup when the script is aborted
 def end_read(signal,frame):
@@ -103,14 +114,14 @@ print "Press Ctrl-C to stop."
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while continue_reading:
-    
-    # Scan for cards    
+
+    # Scan for cards
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # If a card is found
     if status == MIFAREReader.MI_OK:
         print "Card detected"
-    
+
     # Get the UID of the card
     (status,uid) = MIFAREReader.MFRC522_Anticoll()
 
@@ -122,7 +133,7 @@ while continue_reading:
         check_List(uid)
         # This is the default key for authentication
         key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        
+
         # Select the scanned tag
         MIFAREReader.MFRC522_SelectTag(uid)
 
@@ -135,8 +146,7 @@ while continue_reading:
             MIFAREReader.MFRC522_StopCrypto1()
        # else:
             #print "Authentication error"
-        
-            
+
+
         #delay
         time.sleep(2)
-        
